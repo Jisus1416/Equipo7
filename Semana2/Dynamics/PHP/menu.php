d888b463bc64df8c910b3e2661dd1db929c2f601
Regina <regi.garciapor@gmail.com>
2023-05-25 01:37:55 -0600

    feat: agrega maquetado de menu, su css e imágenes que se requieren

---
+++ Semana2/Dynamics/PHP/menu.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../../Statics/media/Escudo.png" type="image/png">
+	<link rel="stylesheet" href="../../Statics/styles/menu.css">
+    <title>Menu</title>
+</head>
+<body>
+    <header class=barra>
+        <img class=escudo src="../../Statics/media/Escudo.png" usemap="escudo" alt="escudo">
+        <a class=prepa6 href="https://www.prepa6.unam.mx/ENP6/_P6/">ENP6</a>
+        <div class=asuntos>
+            <p>Comunidad</p>
+            <p >Ventas</p>
+            <p>Busquedas</p>
+            <p>Objetos perdidos</p>
+        </div>
+        <p class=ayuda>Ayuda</p>
+        <div class=linea>|</div>
+        <a class=registrate href="http://localhost/Proyecto_Curso_Web/Dynamics/PHP/registro.php">Regístrate</a>
+        <a class=iniciaSesion href="http://localhost/Proyecto_Curso_Web/Dynamics/PHP/inicio_sesion.php">Inicia Sesión</a>
+    </header>
+    <p class=recuadro>
+        <h1 class=saludo><strong>¡Bienvenidos al sitio no oficial de la prepa!</strong></h1>
+        <br>
+        <p class=entrada>Este es un espacio creado por la comunidad para la comunidad, creado para convivir con el resto de tu comunidad estudiantil.</p>
+        <img class=foto src="../../Statics/media/fondo_inicio_sesion.jpg">
+    </p>
+    <h2 class=pregunta><strong>¿Qué puedes hacer?<strong></h2>
+    <img class=juegos src="../../Statics/media/Juegos.jpeg">
+    <img class=busqueda src="../../Statics/media/Busqueda.jpeg">
+    <img class=comyven src="../../Statics/media/Compra_y_venta.jpeg">
+    <img class=comunidad src="../../Statics/media/Comunidad.jpeg">
+    <p class=pie>
+        <img class=escudo2 src="../../Statics/media/Escudo.png">
+        <a class=prepa href="https://www.prepa6.unam.mx/ENP6/_P6/">ENP6</a>
+        <p class=acerca>Acerca de nosotros</p>
+        <p class=nota>Ayudanos a mantener el sitio actualizado:</p>
+        <p class=sello>@2023 enp</p>
+    </p>
+
+</body>
+</html>
