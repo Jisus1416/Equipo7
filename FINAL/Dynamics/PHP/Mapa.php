4b006ea338377d3f108b133c8b969be2f266bdc1
Ferchirris <133717281+Ferchirris@users.noreply.github.com>
2023-06-13 20:30:19 -0600

    Add files via upload

---
+++ FINAL/Dynamics/PHP/Mapa.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instalciones</title>
+</head>
+<body>
+<img src="mapita.png" usemap="#image-map">
+
+<map name="image-map">
+    <area target="_parent" alt="Canchas1" title="Canchas1" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/areas_deportivas6.jpg" coords="523,91,732,274" shape="rect">
+    <area target="_parent" alt="Canchas2" title="Canchas2" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/areas_deportivas1.jpg" coords="735,92,894,274" shape="rect">
+    <area target="_parent" alt="Alberca" title="Alberca" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/areas_deportivas3.jpg" coords="979,186,1081,254" shape="rect">
+    <area target="_parent" alt="Fosa" title="Fosa" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/areas_deportivas4.jpg" coords="982,131,1050,182" shape="rect">
+    <area target="_parent" alt="Pimponeras" title="Pimponeras" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/espacios7.jpg" coords="570,338,811,338,637,454" shape="poly">
+    <area target="_parent" alt="Bibloteca1" title="Bibloteca1" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/biblioteca3.jpg" coords="561,281,718,321" shape="rect">
+    <area target="_parent" alt="Bibloteca2" title="Bibloteca2" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/biblioteca1.jpg" coords="850,323,719,272" shape="rect">
+    <area target="_parent" alt="Patio de quintos" title="Patio de quintos" href="https://i.pinimg.com/originals/70/2f/95/702f95b80fab5473d1079fda799078c0.jpg" coords="140,217,369,77,442,191,215,328" shape="poly">
+    <area target="_parent" alt="Patio de cuartos" title="Patio de cuartos" href="https://i.pinimg.com/736x/a3/6c/de/a36cde52769e1763b0b170367f6f20cb--patio.jpg" coords="216,338,451,192,522,304,286,451" shape="poly">
+    <area target="_parent" alt="Patio de sextos" title="Patio de sextos" href="https://i.pinimg.com/736x/54/50/91/54509118b1c84acac5577950c1b1f9ba.jpg" coords="296,455,524,309,589,421,382,543,362,553,323,492" shape="poly">
+    <area target="_parent" alt="Auditorio1" title="Auditorio1" href="https://sic.cultura.gob.mx/imagenes_cache/auditorio_714_i_52746.png" coords="488,501,598,433,633,470,558,580" shape="poly">
+    <area target="_parent" alt="Auditorio2" title="Auditorio2" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/auditorio3.jpg" coords="561,581,643,456,679,511" shape="poly">
+    <area target="_parent" alt="Área verde" title="Área verde" href="https://i.pinimg.com/originals/99/f6/eb/99f6ebb5449bce77a5adeff6bc751691.jpg" coords="134,81,361,74,173,193,145,146" shape="poly">
+    <area target="_parent" alt="Canchitas" title="Canchitas" href="https://www.prepa6.unam.mx/ENP6/_P6/plantel/img/instalaciones/areas_deportivas6.jpg" coords="440,88,519,175" shape="rect">
+</map>
+</body>
+</html>
