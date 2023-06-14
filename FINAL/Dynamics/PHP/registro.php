2a9e5275b8f1a648f299d8724641b005178bd097
JimenaM4 <133259405+JimenaM4@users.noreply.github.com>
2023-06-13 22:15:01 -0600

    FEAT AND FIX: agrega bootstrap y arregla errores

--- FINAL/Dynamics/PHP/registro.php
+++ FINAL/Dynamics/PHP/registro.php
@@ -6,22 +6,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../Statics/media/Escudo.png" type="image/png">
     <link rel="stylesheet" href="../../Statics/styles/registro.css">
-    <title>Registro</title>
+    <link rel="stylesheet" href="../../../../../bootstrap/libs/bootstrap-5.3.0-dist/css/bootstrap.css">
+    <script src="../../../../../bootstrap/libs/bootstrap-5.3.0-dist/js/bootstrap.bundle.js"></script>
+    <title>Registro</title> 
 </head>
 <body>
-    <header class=barra>
-        <img class=escudo src="../../Statics/media/Escudo.png">
-        <p class=prepa6>ENP6</p>
-        <p class=ayuda>Ayuda</p>
-        <div class=linea>||</div>
-        <a class=registrate href="http://localhost/Curso_Web/Proyecto_Curso_Web/Dynamics/PHP/registro.php">Regístrate</a>
-        <a class=iniciaSesion href="http://localhost/Curso_Web/Proyecto_Curso_Web/Dynamics/PHP/inicio_sesion.php">Inicia Sesión</a>
-    </header>
+<header class=barra>
+        <nav class="navbar navbar-expand-lg" id="nav">
+            <div class="container-fluid">
+                <img src="../../Statics/media/Escudo.png" alt="ENP6" width="30" height="24" class="d-inline-block align-text-top text-light" id="escudo">
+                <a class="navbar-brand text-light" href="#">ENP6</a><br>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="search">
+                        <button class="btn btn-outline-success text-light" id="btnBusc" type="submit">Search</button>
+                </form>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light" class="ayuda" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                ayuda
+                            </a>
+                            <ul class="dropdown-menu" id="nav">
+                                <li><a class="dropdown-item text-light" href="#">Terminos y condiciones</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item text-light" href="#">Creditos</a></li>
+                            </ul>       
+                        </li>
+                        <li class="nav-item"> 
+                        <a class="nav-link active text-light" aria-current="page" href="http://localhost/Curso_Web/Proyecto_Curso_Web/General/Dynamics/PHP/inicio_sesion.php">inicia sesión</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link text-light" href="http://localhost/Curso_Web/Proyecto_Curso_Web/General/Dynamics/PHP/registro.php">registrate</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header> 
  	<h1 class=comentario1>Registrate para acceder a la comunidad de la <strong>ENP6</strong></h1>
     <img class=img src="../../Statics/media/Escudo.png">
     <h1 class=comentario2><strong>Te damos la bienvenida a tu sitio web</strong><h1>
     <h4 class=comentario3>Conéctate con tu comunidad</h4>
-    <form class= forms action="./registro.php" method="post" target="_blank">
+    <form class= forms action="./registro.php" method="post" target="_self">
         <div class=datos> 
                 <label for="nombre">
                     <input type="text" name="nombre" placeholder="Nombre" required>
@@ -53,7 +82,7 @@
         <form class= forms action="./registro.php" method="post" target="_blank">
             <div class=opcion>
 	            <h5><center>o</center></h5>
-                <a href="http://localhost/Curso_Web/Proyecto_Curso_Web/Dynamics/PHP/inicio_sesion.php">Inicia Sesión</a>
+                <a id="aRegistro" href="http://localhost/Curso_Web/Proyecto_Curso_Web/General/Dynamics/PHP/inicio_sesion.php">Inicia Sesión</a>
             </div>
         </form>
 <?php
@@ -63,8 +92,8 @@
     $grupo=(isset($_POST['grupo']) && $_POST["grupo"] != "")? $_POST['grupo'] : "Falta Valor";
     $usuario=(isset($_POST['usuario']) && $_POST["usuario"] != "")? $_POST['usuario'] : "Falta Valor";
     $contra=(isset($_POST['contraseña']) && $_POST["contraseña"] != "")? $_POST['contraseña'] : "Falta Valor";
-    $iniciaSesion=(isset($_POST['iniciaSesion']) && $_POST["iniciaSesion"] != "")? $_POST['iniciaSesion'] : "Falta Valor";
-    echo $iniciaSesion;
+    
+    
 
 
 ?>
