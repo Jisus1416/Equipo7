1a04a4b94cea64cc4fe7a9f8a28960e1b3c00494
Jime <jimena008mm@gmail.com>
2023-06-13 22:10:46 -0600

    FEAT AND FIX: añade bootstrap y arregla errores

--- FINAL/Dynamics/PHP/inicio_sesion.php
+++ FINAL/Dynamics/PHP/inicio_sesion.php
@@ -6,33 +6,62 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../Statics/media/Escudo.png" type="image/png">
 	<link rel="stylesheet" href="../../Statics/styles/inicio_sesion.css">
+    <link rel="stylesheet" href="../../../../../bootstrap/libs/bootstrap-5.3.0-dist/css/bootstrap.css">
+    <script src="../../../../../bootstrap/libs/bootstrap-5.3.0-dist/js/bootstrap.bundle.js"></script>
     <title>Inicio de sesión</title>
 </head>
 <body> 
     <header class=barra>
-        <img class=escudo src="../../Statics/media/Escudo.png">
-        <p class=prepa6>ENP6</p>
-        <p class=ayuda>Ayuda</p>
-        <div class=linea>||</div>
-        <a class=registrate href="http://localhost/Curso_Web/Proyecto_Curso_Web/Dynamics/PHP/registro.php">Regístrate</a>
-        <a class=iniciaSesion href="http://localhost/Curso_Web/Proyecto_Curso_Web/Dynamics/PHP/inicio_sesion.php">Inicia Sesión</a>
+        <nav class="navbar navbar-expand-lg" id="nav">
+            <div class="container-fluid">
+                <img src="../../Statics/media/Escudo.png" alt="ENP6" width="30" height="24" class="d-inline-block align-text-top text-light" id="escudo">
+                <a class="navbar-brand text-light" href="#">ENP6</a><br>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="search">
+                        <button class="btn btn-outline-success text-light" id="btnBusc" type="submit">Search</button>
+                </form>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle text-light" class="ayuda" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                ayuda
+                            </a>
+                            <ul class="dropdown-menu" id="nav">
+                                <li><a class="dropdown-item text-light" href="#">Terminos y condiciones</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item text-light" href="#">Creditos</a></li>
+                            </ul>       
+                        </li>
+                        <li class="nav-item"> 
+                        <a class="nav-link active text-light" aria-current="page" href="http://localhost/Curso_Web/Proyecto_Curso_Web/General/Dynamics/PHP/inicio_sesion.php">inicia sesión</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link text-light" href="http://localhost/Curso_Web/Proyecto_Curso_Web/General/Dynamics/PHP/registro.php">registrate</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header> 
 
-    </header>
  	<h1 class=comentario1><strong>BIENVENIDO a la comunidad de Prepa 6</strong>. Pasa a divertirte</h1>
     <img class=img src="../../Statics/media/Escudo.png">
-    <h1 class=comentario2><strong>¡Hola de nuevo!</strong><h1>
-    <h4 class=comentario3>Conétacte con tu comunidad</h4>
-   	<form  class= forms action="./inicio_sesion.php" method="post" target="_blank">
+    <h1 class="comentario2"><strong>¡Hola de nuevo!</strong><h1>
+    <h4 class="comentario3">Conéctacte con tu comunidad</h4>
+   	<form  class= forms action="./inicio_sesion.php" method="post" target="_self">
         <div class=datos>
-            <label><input class=fomulario1 id="usuario" name="usuario" type="text" placeholder="Usuario" required></label><br><br>
-            <label><input class=fomulario1 id="contraseña" name="contraseña" type="password" placeholder="Contraseña" required></label><br><br>
+            <label><input class="fomulario1" id="usuario" name="usuario" type="text" placeholder="Usuario" required></label><br><br>
+            <label><input class="fomulario1" id="contraseña" name="contraseña" type="password" placeholder="Contraseña" required></label><br><br>
 	        <center><button class=boton id="acceder" name="acceder" type="submit" value=1>Acceder</button></center>   
         </div>
     </form>
-    <form align="center" action="./inicio_sesion.php" method="post" target="_blank">
+    <form align="center" action="./inicio_sesion.php" method="post" target="_self">
         <div class=opcion>
-	        <h5><center>¿No tienes una cuenta?</center></h5>
-            <a href="http://localhost/Curso_Web/Proyecto_Curso_Web/Dynamics/PHP/registro.php">Registrate</a>
+	        <h7><center>¿No tienes una cuenta?</center></h7>
+            <a id="aRegistro" href="http://localhost/Curso_Web/Proyecto_Curso_Web/General/Dynamics/PHP/registro.php">Registrate</a>
         </div>
    </form>
 
@@ -41,7 +70,7 @@
     $contra=(isset($_POST['contraseña']) && $_POST["contraseña"] != "")? $_POST['contraseña'] : "Falta Valor";
     $acceder=(isset($_POST['acceder']) && $_POST["acceder"] != "")? $_POST['acceder'] : "Falta Valor";
     $crearcuen=(isset($_POST['crearcuen']) && $_POST["crearcuen"] != "")? $_POST['crearcuen'] : "Falta Valor";
-
+    
    ?>
 </body>
 </html>
